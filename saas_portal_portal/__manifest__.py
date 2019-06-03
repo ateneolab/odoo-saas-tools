b265b9cb587e79b8d8facc5a3a79fb999ebbb98c
feel App Technologies <technology@feelapp.cloud>
2019-06-03 02:24:45 +0000

    [UPD] revert to original

--- saas_portal_portal/__manifest__.py
+++ saas_portal_portal/__manifest__.py
@@ -1,12 +1,14 @@
 {
     'name': 'SaaS Portal Portal',
-    'version': '12.0.1.1.1',
-    'author': "IT-Projects LLC, Ildar Nasyrov, Nicolas JEUDY",
+    'version': '12.0.1.1.2',
+    'author': "IT-Projects LLC, Ildar Nasyrov, Nicolas JEUDY, Coobytec",
     'license': 'LGPL-3',
+    'summary': 'Allows your customers to see their SaaS services from the portal',
     'category': 'SaaS',
     "support": "apps@it-projects.info",
     'website': 'https://it-projects.info',
-    'depends': ['portal', 'saas_portal'],
+    'depends': ['portal',
+                'saas_portal_sale'],
     'data': [
         'views/website_instance_templates.xml',
     ],
